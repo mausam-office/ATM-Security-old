abb6b9830b081d1cdc3000f309e8c533d72e1eb3
Mausam <eng3@ramlaxmangroup.com>
2022-03-08 12:43:25 +0545

    updated the renaming procedures of images for object detection from image classification procedure

--- rename.py
+++ rename.py
@@ -1,38 +1,26 @@
+from itertools import count
 import os
 import cv2
-dataset_path = "D:/Anaconda/Driver monitoring system/dataset/temp/"
-new_dataset_path = "D:/Anaconda/Driver monitoring system/dataset/renamed_temp/"
+import random
+dataset_path = "./image data/downloaded"
+new_dataset_path = "./image data/renamed"
 
 if not os.path.exists(new_dataset_path):
         print(f"Directory unavailable, Creating directory {new_dataset_path}")
         os.mkdir(new_dataset_path)
 
+files = os.listdir(dataset_path)
+#print(len(files))
+random.shuffle(files)
 
-for cls in ["yawn"]:
-    path = os.path.join(dataset_path, cls)
-    new_path = os.path.join(new_dataset_path, cls)
-
-    if not os.path.exists(new_path):
-        print(f"Directory unavailable, Creating directory {new_path}")
-        os.mkdir(new_path)
-    else:
-        print(f"Directory exists: {new_path}")
-
-    files = os.listdir(path)
-    #print(len(files))
-
-    for file in files:
-        img_path = os.path.join(path, file)
-        img = cv2.imread(img_path)
-        if file.endswith('.jpeg'):
-            file = file.replace('.jpeg', '.jpg')
-        elif file.endswith('.png'):
-            file = file.replace('.png', '.jpg')
-        elif file.endswith('.JPG'):
-            file = file.replace('.JPG', '.jpg')
-        else:
-            pass
-        if file.endswith('.jpg'):
-            new_img_path = os.path.join(new_path, file)
-            cv2.imwrite(new_img_path, img)
-            print(new_img_path + " Is saved in new directory.")
+for index, file in enumerate(files):
+    # update the index value if new files are to added in ./image data/remaned/ directory.
+    img_path = os.path.join(dataset_path, file)
+    img = cv2.imread(img_path)
+    project_name='atm'
+    file_name=project_name + str(index) + '.jpg'
+    
+    if file_name.endswith('.jpg'):
+        new_img_path = os.path.join(new_dataset_path, file_name)
+        cv2.imwrite(new_img_path, img)
+        print(new_img_path + " Is saved in new directory.")
