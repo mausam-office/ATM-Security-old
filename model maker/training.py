be2b0a5d466875edfb7023527aaf429616895319
Mausam <eng3@ramlaxmangroup.com>
2022-03-09 16:06:33 +0545

    Updated the training file with spec of pretained model and  labels of atm

--- model maker/training.py
+++ model maker/training.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import time
 
 from tflite_model_maker.config import ExportFormat
 from tflite_model_maker import model_spec
@@ -12,49 +13,26 @@ tf.get_logger().setLevel('ERROR')
 from absl import logging
 logging.set_verbosity(logging.ERROR)
 
-# split data into training and testing set manually
-'''
-import os, random, shutil
-try:
-    os.mkdir('dataset/train')
-    os.mkdir('dataset/test')
-except:
-    print("train test directory already exits.")
-
-
-image_paths = os.listdir('dataset/images')
-random.shuffle(image_paths)
-
-
-for i, image_path in enumerate(image_paths):
-  if i < int(len(image_paths) * 0.8):
-    try:
-        shutil.copy(f'dataset/images/{image_path}', 'dataset/train')
-        shutil.copy(f'dataset/annotations/{image_path.replace("jpg", "xml")}', 'dataset/train')
-    except:
-        shutil.copy(f'dataset/images/{image_path}', 'dataset/train')
-        shutil.copy(f'dataset/annotations/{image_path.replace("jpeg", "xml")}', 'dataset/train')
-  else:
-    try:
-        shutil.copy(f'dataset/images/{image_path}', 'dataset/test')
-        shutil.copy(f'dataset/annotations/{image_path.replace("jpg", "xml")}', 'dataset/test')
-    except:
-        shutil.copy(f'dataset/images/{image_path}', 'dataset/test')
-        shutil.copy(f'dataset/annotations/{image_path.replace("jpeg", "xml")}', 'dataset/test')
-'''
 
 # select model architecture
-spec = model_spec.get('efficientdet_lite0')
+#spec = model_spec.get('efficientdet_lite0')
+spec = object_detector.EfficientDetSpec(
+  model_name='efficientdet-lite0',
+  uri='https://tfhub.dev/tensorflow/efficientdet/lite0/feature-vector/1', 
+  hparams={'max_instances_per_image': 50})
 
 # Load the dataset
-train_data = object_detector.DataLoader.from_pascal_voc('dataset/train', 'dataset/train', ['log'])
-validation_data = object_detector.DataLoader.from_pascal_voc('dataset/test', 'dataset/test', ['log'])
+labels = ['hammer', 'mask', 'person', 'smoke', 'knife', 'robbery mask', 'gun', 'driller', 'smoke', 'keyboard', 'phone', 'screwdriver', 'laptop', 'mouse', 'helmet']
+train_data = object_detector.DataLoader.from_pascal_voc(images_dir='dataset/train', annotations_dir='dataset/train', label_map=labels)
+validation_data = object_detector.DataLoader.from_pascal_voc(images_dir='dataset/test', annotations_dir='dataset/test', label_map=labels)
 
 # Training of tensorflow lite model 
-model = object_detector.create(train_data, model_spec=spec, epochs = 10, batch_size=8, train_whole_model=True, validation_data=validation_data)
+model = object_detector.create(train_data, model_spec=spec, epochs = 1000, batch_size=2, train_whole_model=True, validation_data=validation_data, do_train=True)
 
+time.sleep(10)
 # Model evaluation on test data
 model.evaluate(validation_data)
 
 # Export tensorflow model
-model.export(export_dir='./models/model_10_8_mm')
+model.export(export_dir='./models/atm_best_16_lite0_mm')
+
