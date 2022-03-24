fe2c71e67c669de6d0a1a708c8d005435ebe0288
Mausam <eng3@ramlaxmangroup.com>
2022-03-24 16:10:20 +0545

    Modifided path and image name prefix

--- rename.py
+++ rename.py
@@ -2,8 +2,8 @@ from itertools import count
 import os
 import cv2
 import random
-dataset_path = "./image data/downloaded"
-new_dataset_path = "./image data/renamed"
+dataset_path = "./image data/v2/raw"
+new_dataset_path = "./image data/v2/images"
 
 if not os.path.exists(new_dataset_path):
         print(f"Directory unavailable, Creating directory {new_dataset_path}")
@@ -17,7 +17,7 @@ for index, file in enumerate(files):
     # update the index value if new files are to added in ./image data/remaned/ directory.
     img_path = os.path.join(dataset_path, file)
     img = cv2.imread(img_path)
-    project_name='atm'
+    project_name='atm_v2_'
     file_name=project_name + str(index) + '.jpg'
     
     if file_name.endswith('.jpg'):
